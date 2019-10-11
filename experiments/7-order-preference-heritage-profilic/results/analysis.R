9ae5a4200b84d8fc2c6ac22a5187bfe8a666fe03
Zeinab Kachakeche <zkachake@uci.edu>
2019-10-10 18:53:32 -0700

    Submission link
    Added submission linkn for prolific on the last page

---
+++ experiments/7-order-preference-heritage-profilic/results/analysis.R
@@ -0,0 +1,77 @@
+library(ggplot2)
+library(lme4)
+library(hydroGOF)
+library(dplyr)
+
+setwd("~/git/arabic_adjectives/experiments/4-order-preference-heritage/results/")
+source("helpers.R")
+
+
+df = read.csv("results.csv",header=T)
+    
+d = subset(df, select=c("subject_information_age","subject_information_asses","subject_information_comments","subject_information_describe","subject_information_dialect", "subject_information_education", "subject_information_enjoyment", "subject_information_gender","subject_information_language","subject_information_lived","subject_information_proficiency","subject_information_test1","subject_information_test2","subject_information_test3","subject_information_years","time_in_minutes","trials_class1","trials_class2","trials_gender","trials_noun","trials_nounclass","trials_predicate1","trials_predicate2","trials_response","trials_slide_number","workerID"))
+
+# got all the test questions correct
+d = d[d$subject_information_test1=="correct"&d$subject_information_test2=="correct"&d$subject_information_test3=="correct",]
+# haven't lived in arabic-speaking country after 8
+d = d[d$subject_information_lived!="both"&d$subject_information_lived!="after",]
+## describe as arabic-arabic
+#d = d[d$describe=="arabic-arabic",]
+# levantine arabic
+d = d[d$subject_information_dialect=="levantine",]
+
+#d = d[d$subject_information_language != "English"&d$subject_information_language!="English "&d$subject_information_language!=""&d$subject_information_language!="انجليزي",]
+#d = d[d$asses=="Yes",]
+
+length(unique(d$workerID)) #n=11 (20)
+
+t <- d
+
+#####
+## duplicate observations by first predicate
+#####
+
+library(tidyr)
+
+o <- t
+o$rightpredicate1 = o$trials_predicate2
+o$rightpredicate2 = o$trials_predicate1
+o$rightresponse = 1-o$trials_response
+agr = o %>% 
+  select(trials_predicate1,rightpredicate1,trials_response,rightresponse,workerID,trials_noun,trials_nounclass,trials_class1,trials_class2) %>%
+  gather(predicateposition,trials_predicate,trials_predicate1:rightpredicate1,-workerID,-trials_noun,-trials_nounclass,-trials_class1,-trials_class2)
+agr$correctresponse = agr$trials_response
+agr[agr$predicateposition == "rightpredicate1",]$correctresponse = agr[agr$predicateposition == "rightpredicate1",]$rightresponse
+agr$correctclass = agr$trials_class1
+agr[agr$predicateposition == "rightpredicate1",]$correctclass = agr[agr$predicateposition == "rightpredicate1",]$trials_class2
+head(agr[agr$predicateposition == "rightpredicate1",])
+agr$trails_response = NULL
+agr$rightresponse = NULL
+agr$trials_class1 = NULL
+agr$trials_class2 = NULL
+nrow(agr) #XXX
+#write.csv(agr,"~/git/arabic_adjectives/experiments/2-order-preference-expanded/results/arabic-naturalness-duplicated.csv")
+agr$correctresponse = 1 - agr$correctresponse
+
+agr = agr[!is.na(agr$correctresponse),]
+
+adj_agr = aggregate(correctresponse~trials_predicate*correctclass,FUN=mean,data=agr)
+adj_agr
+
+class_agr = aggregate(correctresponse~correctclass,FUN=mean,data=agr)
+
+class_s = bootsSummary(data=agr, measurevar="correctresponse", groupvars=c("correctclass"))
+#write.csv(class_s,"../results/tagalog_class_s.csv")
+
+ggplot(data=class_s,aes(x=reorder(correctclass,-correctresponse,mean),y=correctresponse))+
+  geom_bar(stat="identity",fill="lightgray",color="black")+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=reorder(correctclass,-correctresponse,mean), width=0.1))+
+  geom_hline(yintercept=0.5,linetype="dashed") + 
+  xlab("\nadjective class")+
+  ylab("preferred\ndistance from noun\n")+
+  ylim(0,1)+
+  #labs("order\npreference")+
+  theme_bw()#+
+#theme(axis.text.x=element_text(angle=90,vjust=0.35,hjust=1))
+#ggsave("../results/class_distance.pdf",height=3)
+#ggsave("../results/LSA_class_distance.png",height=2,width=4.3)
---
+++ experiments/7-order-preference-heritage-profilic/results/convert_json_to_df.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import json
+
+def convert_json_to_df(json_obj):
+  """
+  This functions takes in a (nested) json object as input and converts it into a dataframe.
+  """
+
+  from flatten_json import flatten_preserve_lists
+  from pandas.io.json import json_normalize
+  
+  # default index for json flatten is 3
+  json_flattened = flatten_preserve_lists(json_obj, max_list_index=100)
+  df = json_normalize(json_flattened)
+  
+  return df
+
+
+
+#load data and name it json_trials1
+with open('new_map_data.json') as json_file:
+    
+    # add "[" as first element, remove last comma from end of string, then add "]" to end of string
+    # json_file is an _io.textiowrapper object and the .read() method is converting it to a string
+    json_file_string = '[' + json_file.read()[:-1] + ']'
+
+    #json.loads expects a string as an iput 
+    json_trials1 = json.loads(json_file_string)
+
+df_trials1 = pd.DataFrame() #this creates an empty dataframe
+for x in json_trials1:
+  df_ = convert_json_to_df(x)
+  df_trials1 = pd.concat([df_trials1, df_])
+
+df_trials1.to_csv("results.csv")
+
+# print(json_trials1)
